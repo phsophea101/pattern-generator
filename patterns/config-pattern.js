5deaa2a74eb22ff78a49da0055d7006e80750a15
Sophea PHOS <52313314+phsophea101@users.noreply.github.com>
2022-09-30 15:47:20 +0700

    Add files via upload

--- patterns/config-pattern.js
+++ patterns/config-pattern.js
@@ -2,6 +2,11 @@ import * as mongoMVC from './mvc/mongo.js';
 import * as jpaMVC from './mvc/jpa.js';
 import * as sampleMVC from './mvc/sample.js';
 
+const isNotEmptyFor = (prompt) => (value) => {
+	if (!value) return prompt + " is required";
+	return true;
+};
+
 export default function (plop) {
 
 	plop.setHelper('genPackage', function (text) {
@@ -13,27 +18,35 @@ export default function (plop) {
 			{
 				type: 'input',
 				name: 'package',
-				message: "package's name?"
+				message: "package's name?",
+				validate: isNotEmptyFor("package"),
 			},
 			{
 				type: 'input',
 				name: 'service',
-				message: "module's name?"
+				message: "module's name?",
+				validate: isNotEmptyFor("service"),
 			},
 			{
-				type: 'input',
+				type: 'rawlist',
 				name: 'pattern',
-				message: "what's your pattern?"
+				message: "Choose your pattern?",
+				choices: [
+					{ name: "MVC pattern", value: "mvc" },
+					{ name: "Hexagonal pattern", value: "hexagonal" }
+				],
+				validate: isNotEmptyFor("pattern"),
 			},
 			{
 				type: 'input',
 				name: 'data',
-				message: "what's your data?"
+				message: "what's your data?",
+				validate: isNotEmptyFor("data"),
 			}
 		],
 		actions: function (data) {
 			var actions = [];
-			var pattern = sampleMVC;
+			var pattern = null;;
 			if (data.pattern == 'mvc') {
 				if (data.data == 'mongo') {
 					pattern = mongoMVC;
@@ -42,6 +55,10 @@ export default function (plop) {
 				} else {
 					pattern = sampleMVC;
 				}
+			}
+			if (pattern == null) {
+				console.log('\x1B[31m*Oop, something went wrong! we will back to fix soon.\x1B[34m -_-');
+				return actions;
 			}
 			for (var i = 0; i < pattern.actions.length; i++) {
 				actions.push({
